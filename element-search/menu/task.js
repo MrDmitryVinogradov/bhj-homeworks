3b57b136f1c02b467702a56b7567208961a0e4c4
Dmitry Vinogradov <mr.dmitryvinogradov@gmail.com>
2021-07-01 10:59:32 +0300

    element search fix

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,9 +2,17 @@ let menuLinks = document.querySelectorAll('.menu__link');
 let len = menuLinks.length;
 for (let i = 0; i < len; i++) {
     menuLinks[i].onclick = () => {
-        menuLinks[i].closest('.menu__item').querySelector('.menu_sub').classList.add('menu_active');
-        if (menuLinks[i].closest('.menu__item').querySelector('.menu_sub') != null) {
-            menuLinks[i].onclick = event.preventDefault();
+        if (menuLinks[i].closest('.menu__item').querySelector('.menu_sub').classList.contains('menu_active') === false) {
+            menuLinks[i].closest('.menu__item').querySelector('.menu_sub').classList.add('menu_active');
+            if (menuLinks[i].closest('.menu__item').querySelector('.menu_sub') != null) {
+                return false;
+            }
+        }
+        else if (menuLinks[i].closest('.menu__item').querySelector('.menu_sub').classList.contains('menu_active')) {
+            menuLinks[i].closest('.menu__item').querySelector('.menu_sub').classList.remove('menu_active');
+            if (menuLinks[i].closest('.menu__item').querySelector('.menu_sub') != null) {
+                return false;
+            }
         }
     }
 }
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,31 +1,18 @@
 let sliderItems = Array.from(document.querySelectorAll('.slider__item'));
-let activeIndex = sliderItems.findIndex(element => element.className == 'slider__item slider__item_active');
 let lastItem = sliderItems.length - 1;
 const arrowPrev = document.querySelector('.slider__arrow_prev');
 const arrowNext = document.querySelector('.slider__arrow_next');
 
 let sliderClickLeft = () => {
-    if (activeIndex === 0) {
-        sliderItems[lastItem].classList.add('slider__item_active');
-        sliderItems[0].classList.remove('slider__item_active');
-    }
-    else {
-        sliderItems[activeIndex].classList.remove('slider__item_active');
-        sliderItems[activeIndex - 1].classList.add('slider__item_active');
-    }
-    activeIndex = sliderItems.findIndex(element => element.className == 'slider__item slider__item_active');
+    let index = sliderItems.findIndex(element => element.classList.contains('slider__item_active'));
+    sliderItems[index].classList.remove('slider__item_active');
+    (index === 0 ? sliderItems[lastItem].classList.add('slider__item_active') : sliderItems[index - 1].classList.add('slider__item_active'))
 }
 
 let sliderClickRight = () => {
-    if (activeIndex === lastItem) {
-        sliderItems[0].classList.add('slider__item_active');
-        sliderItems[activeIndex].classList.remove('slider__item_active');
-    }
-    else {
-        sliderItems[activeIndex].classList.remove('slider__item_active');
-        sliderItems[activeIndex + 1].classList.add('slider__item_active');
-    }
-    activeIndex = sliderItems.findIndex(element => element.className == 'slider__item slider__item_active');
+    let index = sliderItems.findIndex(element => element.classList.contains('slider__item_active'));
+    sliderItems[index].classList.remove('slider__item_active');
+    (index === lastItem ? sliderItems[0].classList.add('slider__item_active') : sliderItems[index + 1].classList.add('slider__item_active'))
 }
 
 arrowPrev.onclick = sliderClickLeft;
