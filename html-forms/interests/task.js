a68c5658df85d511e104074f52fadedaa61f5db3
Dmitry Vinogradov <mr.dmitryvinogradov@gmail.com>
2021-07-18 17:41:42 +0300

    1, 2

---
+++ html-forms/interests/task.js
@@ -0,0 +1,15 @@
+let interests = document.querySelector('ul');
+let checkboxes = Array.from(document.querySelectorAll('.interest__check'));
+interests.onclick = () => {
+    for (let i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].onchange = () => {
+            if (checkboxes[i].checked == true) {
+                Array.from(checkboxes[i].closest('.interest').querySelectorAll('.interest__check')).forEach(element => element.checked = true);
+            }
+            if (checkboxes[i].checked == false) {
+                Array.from(checkboxes[i].closest('.interest').querySelectorAll('.interest__check')).forEach(element => element.checked = false);
+            }
+        }
+    }
+}
+
