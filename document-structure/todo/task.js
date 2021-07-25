e089f6d91061ceeb826e04297ed96d9708ec035c
Dmitry Vinogradov <mr.dmitryvinogradov@gmail.com>
2021-07-25 12:57:23 +0300

    dodelal

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,16 +1,18 @@
 let taskInput = document.querySelector('.tasks__input');
 let tasksList = document.querySelector('.tasks__list');
-let tasks = [];
-let closingButtons = [];
 document.addEventListener('keydown', function (e) {
     if (taskInput.value && e.key == 'Enter') {
         let task = document.createElement('div');
         task.innerHTML = '<div class=\"task__title\"> </div> <a href=\"#\" class=\"task__remove\">&times;</a>';
-        tasksList.appendChild(task);
         task.classList.add('task');
         let taskTitle = task.querySelector('.task__title');
         taskTitle.innerText = taskInput.value;
+        tasksList.appendChild(task);
         taskInput.value = '';
-        tasks.push(task);
+        e.preventDefault();
+        task.querySelector('.task__remove').onclick = () => {
+            task.remove();
+        }
     }
 });
+
